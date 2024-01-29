c99e56fa349ac594853009b8c535aefd953033fc
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-01-29 16:53:31 +0300

    => saved

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,38 @@ import './App.scss'
 
 function App() {
 
+    const [value, setValue] = useState(0)
+
+    function incrementOnClickHandler() {
+        setValue(value + 1)
+    }
+
+    function setLocalStorageOnClickHandler() {
+        localStorage.setItem('value', JSON.stringify(value))
+    }
+
+    function getLocalStorageOnClickHandler() {
+        let valueAsString = localStorage.getItem('value')
+        if (valueAsString) {
+            setValue(JSON.parse(valueAsString))
+        }
+    }
+
+    function clearLocalStorageOnClickHandler() {
+        localStorage.clear()
+        setValue(0)
+    }
+
+
     return (
         <div className={'App'}>
-            123
+            <h1>{value}</h1>
+            <button onClick={incrementOnClickHandler}>INC</button>
+            <button onClick={setLocalStorageOnClickHandler}>SET</button>
+            <button onClick={getLocalStorageOnClickHandler}>GET</button>
+            <button onClick={clearLocalStorageOnClickHandler}>CLEAR</button>
         </div>
     )
 }
 
 export default App
-
-
-
