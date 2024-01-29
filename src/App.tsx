3fb0627236bbd6bf3c388683effdc02d3c4b3b0e
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-01-29 18:10:35 +0300

    => saved

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.scss'
 
 //======================================================================================================
@@ -8,36 +8,28 @@ import './App.scss'
 
 function App() {
 
-    const [value, setValue] = useState(0)
+    const [value, setValue] = useState(() => {
+        let valueAsString = localStorage.getItem('value')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            return newValue
+        } else return 1
+    })
+
+    useEffect(() => {
+        localStorage.setItem('value', JSON.stringify(value))
+
+    }, [value])
 
     function incrementOnClickHandler() {
         setValue(value + 1)
     }
 
-    function setLocalStorageOnClickHandler() {
-        localStorage.setItem('value', JSON.stringify(value))
-    }
-
-    function getLocalStorageOnClickHandler() {
-        let valueAsString = localStorage.getItem('value')
-        if (valueAsString) {
-            setValue(JSON.parse(valueAsString))
-        }
-    }
-
-    function clearLocalStorageOnClickHandler() {
-        localStorage.clear()
-        setValue(0)
-    }
-
-
     return (
         <div className={'App'}>
             <h1>{value}</h1>
             <button onClick={incrementOnClickHandler}>INC</button>
-            <button onClick={setLocalStorageOnClickHandler}>SET</button>
-            <button onClick={getLocalStorageOnClickHandler}>GET</button>
-            <button onClick={clearLocalStorageOnClickHandler}>CLEAR</button>
+
         </div>
     )
 }
